321914ffe375e6b5ef1e715a6cd8adf1ecefde62
hanzy88 <286688787@qq.com>
2019-09-06 09:28:54 +0800

    update for cmake

--- myPro/src/CMakeFiles/generate-spirv.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generate-spirv.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- myPro/src/CMakeFiles/ncnn.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libncnn.a"
-)
